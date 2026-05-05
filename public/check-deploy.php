374b8bb7edf048212a4c80f23740937ef29e2d46
esolyn <esolynellis@gmail.com>
2026-05-06 06:43:30 +0800

    Update check-deploy.php to verify brand_name deployment

--- public/check-deploy.php
+++ public/check-deploy.php
@@ -1,18 +1,26 @@
 <?php
 if (($_GET['key'] ?? '') !== 'dona2025') { http_response_code(403); die(); }
 
-$middleware = file_get_contents(__DIR__ . '/../app/Http/Middleware/SetLocaleShopApi.php');
-$deployed = strpos($middleware, 'Mongolia-only') !== false;
+// Check if ProductSimple.php on server has brand_name
+$resource = file_get_contents(__DIR__ . '/../beike/Shop/Http/Resources/ProductSimple.php');
+$hasBrandName = str_contains($resource, "'brand_name'");
 
+// Check ProductRepo has brand_name filter
+$repo = file_get_contents(__DIR__ . '/../beike/Repositories/ProductRepo.php');
+$hasBrandFilter = str_contains($repo, 'brand_name filter');
+
+// Check last deploy log
 $logFile = __DIR__ . '/../storage/logs/auto-deploy.log';
 $lastLog = '';
 if (file_exists($logFile)) {
     $lines = file($logFile);
-    $lastLog = implode('', array_slice($lines, -20));
+    $lastLog = implode('', array_slice($lines, -10));
 }
 
 header('Content-Type: application/json');
 echo json_encode([
-    'locale_fix_deployed' => $deployed,
-    'last_deploy_log' => $lastLog,
+    'resource_has_brand_name'  => $hasBrandName,
+    'repo_has_brand_filter'    => $hasBrandFilter,
+    'resource_modified'        => date('Y-m-d H:i:s', filemtime(__DIR__ . '/../beike/Shop/Http/Resources/ProductSimple.php')),
+    'last_deploy_log'          => $lastLog,
 ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
