7ab9c168d53dcb91b27e832dae26792ee9e5ac5f
esolyn <esolynellis@gmail.com>
2026-05-05 21:41:40 +0800

    Read main nginx conf

--- public/server-config.php
+++ public/server-config.php
@@ -1,35 +1,6 @@
 <?php
 if (($_GET['key'] ?? '') !== 'dona2025') { http_response_code(403); die(); }
 
-$results = [];
-
-// Search all possible locations
-$searchDirs = [
-    '/www/server/nginx/conf/vhost',
-    '/www/server/panel/vhost/nginx',
-    '/www/server/nginx/conf',
-];
-
-foreach ($searchDirs as $dir) {
-    $handle = @opendir($dir);
-    if (!$handle) { $results[$dir] = 'cannot open'; continue; }
-    $files = [];
-    while (($f = readdir($handle)) !== false) {
-        if ($f === '.' || $f === '..') continue;
-        $path = "$dir/$f";
-        $c = @file_get_contents($path);
-        if ($c && (stripos($c, 'dona') !== false || stripos($c, '103.168') !== false)) {
-            $results['FOUND'] = $path;
-            $results['gzip'] = strpos($c, 'gzip on') !== false ? 'YES' : 'NO';
-            $results['expires'] = strpos($c, 'expires') !== false ? 'YES' : 'NO';
-            $results['writable'] = is_writable($path) ? 'YES' : 'NO';
-            $results['preview'] = substr($c, 0, 1500);
-        }
-        $files[] = $f;
-    }
-    closedir($handle);
-    $results[$dir] = $files;
-}
-
-header('Content-Type: application/json');
-echo json_encode($results, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+// Read main nginx.conf
+$nginxConf = file_get_contents('/www/server/nginx/conf/nginx.conf');
+echo "<pre>" . htmlspecialchars($nginxConf) . "</pre>";
